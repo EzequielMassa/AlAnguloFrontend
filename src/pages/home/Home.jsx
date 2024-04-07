57c02d9df6a91762ce46fb091a187b9f90e709aa
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-04-07 10:55:58 -0300

    added soccerfield filter to home , created helper capitalizeword

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
+import SoccerFieldFilter from '../../components/SoccerFieldFilter/SoccerFieldFilter'
+import SoccerFieldFilterCard from '../../components/SoccerFieldFilterCard/SoccerFieldFilterCard'
+
 export const Home = () => {
+	const [queryResult, setQueryResult] = useState([])
+	const handleQueryResultChange = (newQueryResult) => {
+		setQueryResult(newQueryResult)
+	}
+
 	return (
 		<>
 			<h1>Home</h1>
+			<article className='container-md '>
+				<section className='row'>
+					<div className='col-12'>
+						<SoccerFieldFilter onQueryResultChange={handleQueryResultChange} />
+						{queryResult ? (
+							queryResult.map((result) => (
+								<SoccerFieldFilterCard key={result._id} soccerField={result} />
+							))
+						) : (
+							<></>
+						)}
+					</div>
+				</section>
+			</article>
 		</>
 	)
 }
