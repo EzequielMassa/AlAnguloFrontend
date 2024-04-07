a8523332beb11bf06dc11d7c496681482698f02b
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-04-07 16:51:38 -0300

    added soccerfield cards to home

---
+++ src/components/SoccerFieldCards/SoccerFieldCards.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import SoccerFieldCard from '../SoccerFieldCard/SoccerFieldCard'
+
+function SoccerFieldCards() {
+	const [soccerFields, setSoccerFields] = useState([])
+	const fetchSoccerFields = async () => {
+		const response = await fetch('http://localhost:4000/api/soccerfields')
+		const data = await response.json()
+		if (data.data) {
+			const relevants = data.data.slice(0, 3)
+			setSoccerFields(relevants)
+		}
+	}
+	console.log(soccerFields)
+
+	useEffect(() => {
+		fetchSoccerFields()
+	}, [])
+
+	return (
+		<>
+			<div className='d-flex justify-content-center gap-2  '>
+				{soccerFields ? (
+					soccerFields.map((field) => (
+						<SoccerFieldCard key={field._id} fieldImage={field.imgUrl} />
+					))
+				) : (
+					<></>
+				)}
+			</div>
+		</>
+	)
+}
+export default SoccerFieldCards
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import SoccerFieldFilter from '../../components/SoccerFieldFilter/SoccerFieldFilter'
 import SoccerFieldFilterCard from '../../components/SoccerFieldFilterCard/SoccerFieldFilterCard'
+import SoccerFieldCards from '../../components/SoccerFieldCards/SoccerFieldCards'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
 	const [queryResult, setQueryResult] = useState([])
@@ -11,8 +13,11 @@ export const Home = () => {
 	return (
 		<>
 			<h1>Home</h1>
-			<article className='container-md '>
+			<article className='container-md my-4'>
 				<section className='row'>
+					<h2 className='text-center title py-4'>
+						Encontra la cancha que necesitas rapidamente
+					</h2>
 					<div className='col-12'>
 						<SoccerFieldFilter onQueryResultChange={handleQueryResultChange} />
 						{queryResult ? (
@@ -22,6 +27,17 @@ export const Home = () => {
 						) : (
 							<></>
 						)}
+					</div>
+				</section>
+			</article>
+			<article className='container-md my-4'>
+				<section className='row'>
+					<h3 className='text-center subtitle'>Descubri nuestras canchas</h3>
+					<div className='col-12 d-flex flex-column align-items-center gap-3'>
+						<SoccerFieldCards />
+						<Link className='btn btn-primary ' to={'/canchas'}>
+							Explora nuestras canchas
+						</Link>
 					</div>
 				</section>
 			</article>
