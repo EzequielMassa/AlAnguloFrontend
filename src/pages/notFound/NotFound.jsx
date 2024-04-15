225ca95734ba054a28b15e4c8f3192a548060cfd
Facundo Gimenez <facuie014@gmail.com>
2024-04-15 02:08:30 -0300

    feature 404

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,7 +1,26 @@
+import './NotFound.css'
 export const NotFound = () => {
 	return (
-		<>
-			<h1>NotFound</h1>
-		</>
+		<main class="main">
+            <section class="home">
+                <div class="home__container container">
+                    <div class="home__data">
+                        <span class="home__subtitle">Error 404</span>
+                        <h1 class="home__title">Hey Buddy</h1>
+                        <p class="home__description">
+                            We can't seem to find the page <br/> you are looking for.
+                        </p>
+                        <a href="#" class="home__button">
+                            Go Home
+                        </a>
+                    </div>
+
+                    <div class="home__img">
+                        <img src="assets/img/ghost-img.png" alt=""/>
+                        <div class="home__shadow"></div>
+                    </div>
+                </div>
+            </section>
+        </main>
 	)
 }
