251c20d42b6f3ff66a7bc17d130db8015f43bab6
Facundo Gimenez <facuie014@gmail.com>
2024-04-15 21:54:32 -0300

    404 Page

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,23 +1,25 @@
+import Spinner from '../../components/Spinner/Spinner'
+import SpinnerAlternative from '../../components/SpinnerAlternative/SpinnerAlternative'
 import './NotFound.css'
+import { Link } from 'react-router-dom'
 export const NotFound = () => {
 	return (
 		<main class="main">
-            <section class="home">
-                <div class="home__container container ">
+            <section class="home404">
+                <div class="home__container d-flex flex-column align-items-center flex-md-row justify-content-around">
                     <div class="home__data">
-                        <span class="home__subtitle">Error 404</span>
-                        <h1 class="home__title">Hey Buddy</h1>
+                        <h1 class="home__title">Lo Sentimos</h1>
                         <p class="home__description">
-                            We can't seem to find the page <br/> you are looking for.
+                            No encontramos la pagina <br/> que estas buscando.
                         </p>
-                        <a href="#" class="home__button">
-                            Go Home
-                        </a>
+                        <Link to="./" class="home__button">
+                            Volver al Inicio
+                        </Link>
                     </div>
-
-                    <div class="home__img">
-                        <img src="src\assets\images\logo_AlAngulo.png" alt=""/>
-                        <div class="home__shadow"></div>
+                    <div class="home__img d-flex align-items-start">
+                        <h3 className='number404 pt-4'>4</h3>
+                        <SpinnerAlternative/>
+                        <h3 className='number404 pt-4'>4</h3>
                     </div>
                 </div>
             </section>
