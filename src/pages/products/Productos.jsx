f0744f56d1ab8f84ef9749eab20879991e73b08b
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-04-16 19:12:39 -0300

    implemented products context and filters in products page

--- src/pages/products/Productos.jsx
+++ src/pages/products/Productos.jsx
@@ -1,160 +1,100 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import {
-	Row,
+	Button,
+	Card,
 	Col,
 	Container,
-	Card,
-	Button,
-	Navbar,
-	Nav,
 	Form,
 	FormControl,
-	Dropdown,
-	DropdownButton,
+	Nav,
+	Navbar,
+	Row,
 } from 'react-bootstrap'
-import imagen from './camiseta.jpg'
-import './products.css'
-import './filter.css'
-import { NavLink } from 'react-router-dom'
-import { ScrollRestoration } from 'react-router-dom'
-import logo from '../../assets/images/logo_AlAngulo.png'
+import { TiDelete } from 'react-icons/ti'
+import {
+	NavLink,
+	ScrollRestoration,
+	useNavigate,
+	useSearchParams,
+} from 'react-router-dom'
 import { useProductsContext } from '../../context/ProductsContext'
+import { formatCurrency } from '../../helpers/formatCurrency'
+import './filter.css'
+import './products.css'
 function Products() {
-	const [productos, setProductos] = useState([
-		{
-			id: 1,
-			nombre: 'Camisa',
-			categoria: 'remeras',
-			precio: 20.99,
-			cantidad: 50,
-			descripcion: 'Camisa de algodón de manga larga',
-		},
-		{
-			id: 2,
-			nombre: 'Pantalón',
-			categoria: 'remeras',
-			precio: 34.99,
-			cantidad: 30,
-			descripcion: 'Pantalón vaquero de estilo casual',
-		},
-		{
-			id: 3,
-			nombre: 'Zapatos',
-			categoria: 'pantalones',
-			precio: 49.99,
-			cantidad: 20,
-			descripcion: 'Zapatos de cuero formales',
-		},
-		{
-			id: 4,
-			nombre: 'Reloj',
-			categoria: 'remeras',
-			precio: 99.99,
-			cantidad: 15,
-			descripcion: 'Reloj analógico de acero inoxidable',
-		},
-		{
-			id: 5,
-			nombre: 'Mochila',
-			categoria: 'remeras',
-			precio: 39.99,
-			cantidad: 25,
-			descripcion: 'Mochila resistente',
-		},
-		{
-			id: 6,
-			nombre: 'Gafas de sol',
-			categoria: 'panalones',
-			precio: 15.99,
-			cantidad: 40,
-			descripcion: 'Gafas de sol con montura de plástico',
-		},
-		{
-			id: 7,
-			nombre: 'Botella de agua',
-			categoria: 'remeras',
-			precio: 5.99,
-			cantidad: 60,
-			descripcion: 'Botella de agua reutilizable de 500ml',
-		},
-		{
-			id: 8,
-			nombre: 'Libreta',
-			categoria: 'pantalones',
-			precio: 3.99,
-			cantidad: 100,
-			descripcion: 'Libreta de tamaño A5 con hojas rayadas',
-		},
-		{
-			id: 9,
-			nombre: 'Lápices de colores',
-			categoria: 'remeras',
-			precio: 7.99,
-			cantidad: 50,
-			descripcion: 'Set de 12 lápices de colores',
-		},
-		{
-			id: 10,
-			nombre: 'Bolígrafos',
-			categoria: 'pantalones',
-			precio: 4.99,
-			cantidad: 80,
-			descripcion: 'Set de 10 bolígrafos de tinta azul',
-		},
-		{
-			id: 11,
-			nombre: 'Cámara',
-			categoria: 'pantalones',
-			precio: 199.99,
-			cantidad: 10,
-			descripcion: 'Cámara digital compacta de 20MP',
-		},
-		{
-			id: 12,
-			nombre: 'Auriculares',
-			categoria: 'remeras',
-			precio: 29.99,
-			cantidad: 35,
-			descripcion: 'Auriculares inalámbricos ',
-		},
-		// Agrega más productos según sea necesario
-	])
-	const [filteredProducts, setFilteredProducts] = useState([...productos])
-	const [searchTerm, setSearchTerm] = useState('')
-	const [selectedCategory, setSelectedCategory] = useState('')
-	const [sortOrder, setSortOrder] = useState('')
+	const { products, getAllProducts, categories, getAllCategories } =
+		useProductsContext()
+	const [searchParams, setSearchParams] = useSearchParams({})
+	const navigate = useNavigate()
 
-	const handleSearch = () => {
-		const filtered = productos.filter((producto) => {
-			const nameMatches = producto.nombre
-				.toLowerCase()
-				.includes(searchTerm.toLowerCase())
-			const categoryMatches = selectedCategory
-				? producto.categoria === selectedCategory
-				: true
-			return nameMatches && categoryMatches
-		})
+	const applyFilters = () => {
+		const name = searchParams.get('nombre')
+		const price = searchParams.get('precio')
+		const category = searchParams.get('categoria')
 
-		setFilteredProducts(filtered)
+		let filteredFields = products
+		console.log(filteredFields)
+
+		if (name) {
+			filteredFields = filteredFields.filter((field) =>
+				field.name.toLowerCase().includes(name.toLowerCase())
+			)
+		}
+
+		if (price) {
+			if (price === 'menor') {
+				filteredFields = filteredFields.sort((a, b) => a.price - b.price)
+			} else if (price === 'mayor') {
+				filteredFields = filteredFields.sort((a, b) => b.price - a.price)
+			}
+		}
+
+		if (category) {
+			filteredFields = filteredFields.filter(
+				(field) => field.category.name.toLowerCase() === category
+			)
+		}
+
+		return filteredFields
 	}
 
-	const handleClearFilters = () => {
-		setSearchTerm('')
-		setSelectedCategory('')
-		setSortOrder('')
-		setFilteredProducts([...productos])
+	const filteredProducts = applyFilters()
+
+	const setFilters = (name, price, category) => {
+		const params = new URLSearchParams()
+
+		if (name) {
+			params.set('nombre', name)
+		}
+
+		if (price) {
+			params.set('precio', price)
+		}
+
+		if (category) {
+			params.set('categoria', category)
+		}
+
+		setSearchParams(params)
 	}
 
-	const handleSort = (order) => {
-		setSortOrder(order)
-		const sorted = [...filteredProducts].sort((a, b) => {
-			const priceA = a.precio
-			const priceB = b.precio
-			return order === 'asc' ? priceA - priceB : priceB - priceA
-		})
-
-		setFilteredProducts(sorted)
+	const clearProductsFilter = () => {
+		navigate('/productos')
 	}
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target
+		setFilters(
+			name === 'name' ? value : searchParams.get('nombre'),
+			name === 'price' ? value : searchParams.get('precio'),
+			name === 'category' ? value : searchParams.get('categoria')
+		)
+	}
+
+	useEffect(() => {
+		getAllProducts()
+		getAllCategories()
+	}, [])
 
 	return (
 		<>
@@ -167,39 +107,54 @@ function Products() {
 							<Form className='form-search flex-column flex-md-row gap-2 w-100 d-flex justify-content-center align-items-center'>
 								<FormControl
 									type='text'
-									onChangeCapture={handleSearch}
+									name='name'
 									placeholder='Buscar por nombre'
 									className='mr-sm-2 nav-search'
-									value={searchTerm}
-									onChange={(e) => setSearchTerm(e.target.value)}
+									value={searchParams.get('nombre') || ''}
+									onChange={handleInputChange}
 								/>
-								<Button
-									className='btn-search align-self-center d-flex mb-2 ms-2'
-									variant='outline-success'
-									onClick={handleSearch}>
-									Buscar
-								</Button>
-								<DropdownButton
-									title='Categoría'
-									onSelect={(category) => setSelectedCategory(category)}>
-									<Dropdown.Item eventKey='remeras'>Remeras</Dropdown.Item>
-									<Dropdown.Item eventKey='pantalones'>
-										Pantalones
-									</Dropdown.Item>
-								</DropdownButton>
-								<DropdownButton title='Ordenar por precio'>
-									<Dropdown.Item onClick={() => handleSort('asc')}>
-										Menor a mayor
-									</Dropdown.Item>
-									<Dropdown.Item onClick={() => handleSort('desc')}>
-										Mayor a menor
-									</Dropdown.Item>
-								</DropdownButton>
+								<Form.Group
+									className='mb-3  filter_form_group_container'
+									controlId='category'>
+									<Form.Label>Categoria</Form.Label>
+									<Form.Select
+										aria-label='category'
+										name='category'
+										onChange={handleInputChange}
+										value={searchParams.get('categoria') || ''}>
+										<option value=''>Todos</option>
+										{categories ? (
+											categories.map((category) => (
+												<option
+													key={category._id}
+													value={category.name.toLowerCase()}>
+													{category.name}
+												</option>
+											))
+										) : (
+											<></>
+										)}
+									</Form.Select>
+								</Form.Group>
+								<Form.Group
+									className='mb-3 filter_form_group_container '
+									controlId='price'>
+									<Form.Label>Precio</Form.Label>
+									<Form.Select
+										aria-label='price'
+										name='price'
+										onChange={handleInputChange}
+										value={searchParams.get('precio') || ''}>
+										<option hidden>Todos</option>
+										<option value='menor'>Menor precio</option>
+										<option value='mayor'>Mayor precio</option>
+									</Form.Select>
+								</Form.Group>
+								<TiDelete
+									className='d-inline-block  filter_form_icon'
+									onClick={clearProductsFilter}
+								/>
 							</Form>
-
-							<Button variant='outline-secondary' onClick={handleClearFilters}>
-								Limpiar Filtros
-							</Button>
 						</Nav>
 					</Navbar.Collapse>
 				</Navbar>
@@ -209,18 +164,18 @@ function Products() {
 					<div className='text-center no-product'>No hay productos</div>
 				) : (
 					<Row className=' product-row  '>
-						{filteredProducts.map((p, index) => (
-							<Col xl={3} md={4} key={index} className='product-column '>
+						{filteredProducts.map((p) => (
+							<Col xl={3} md={4} key={p._id} className='product-column '>
 								<Card
 									key={p.id}
 									className='card-products my-2 d-flex flex-column  '>
-									<Card.Img variant='top' className='camiseta' src={imagen} />
+									<Card.Img variant='top' className='camiseta' src={p.image} />
 
 									<Card.Body className='text-center bg-white'>
 										<div className='divisor-line' />
-										<Card.Title className='btn-product'>{p.nombre}</Card.Title>
+										<Card.Title className='btn-product'>{p.name}</Card.Title>
 										<Card.Text className='btn-product btn-price'>
-											${p.precio}
+											{formatCurrency(p.price)}
 										</Card.Text>
 										<div className='btn-container d-flex'>
 											<NavLink to='/ProductDetail'>
@@ -232,7 +187,7 @@ function Products() {
 									</Card.Body>
 								</Card>
 							</Col>
-						))}{' '}
+						))}
 					</Row>
 				)}
 			</Container>
